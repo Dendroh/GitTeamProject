7eeba84d0cdfa5a632ab35f22cd66f4415fc8f84
윤동현 <eatpaper1429@gmail.com>
2023-03-10 21:33:45 +0900

    Tripple func 추가

---
+++ GitTeamProj/src/main/java/org/example/calcurator/Tripplefunc.java
@@ -0,0 +1,15 @@
+package org.example.calcurator;
+
+public class Tripplefunc {
+  int left;
+  int right;
+
+  public Tripplefunc(int left, int right) {
+    this.left = left;
+    this.right = right;
+  }
+
+  public int result() {
+    return (left * 3);
+  }
+}

e273a69e3df119e926433592c39cfe8e61b86007
장현준 <gw0406@naver.com>
2023-03-10 20:40:28 +0900

    Minfunc 변경

--- GitTeamProj/src/main/java/org/example/calcurator/Minfunc.java
+++ GitTeamProj/src/main/java/org/example/calcurator/Minfunc.java
@@ -1,4 +1,15 @@
 package org.example.calcurator;
 
 public class Minfunc {
+  int left;
+  int right;
+
+  public Minfunc(int left, int right) {
+    this.left = left;
+    this.right = right;
+  }
+
+  public int result() {
+    return left < right ? left : right;
+  }
 }

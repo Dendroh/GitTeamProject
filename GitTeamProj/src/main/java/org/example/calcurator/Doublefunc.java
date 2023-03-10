e7f833cb4b14d154b9f276603625949f5675e4c2
윤동현 <eatpaper1429@gmail.com>
2023-03-10 21:22:14 +0900

    Double func add

---
+++ GitTeamProj/src/main/java/org/example/calcurator/Doublefunc.java
@@ -0,0 +1,16 @@
+package org.example.calcurator;
+
+public class Doublefunc {
+
+  int left;
+  int right;
+
+  public Doublefunc(int left, int right) {
+    this.left = left;
+    this.right = right;
+  }
+
+  public int result() {
+    return left + right;
+  }
+}

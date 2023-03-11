88858392d130ca57a603ccbf068605bb6b81d662
윤동현 <eatpaper1429@gmail.com>
2023-03-11 18:07:12 +0900

    error

---
+++ GitTeamProj/src/main/java/org/example/calcurator/Main.java
@@ -0,0 +1,47 @@
+package org.example;
+
+
+class calculator{
+    int a,b;
+
+    public calculator(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int Addfunc(int a, int b){
+        //To-Do implementation
+        return 0;
+    }
+
+    public int Divfunc(int a, int b){
+        //To-Do implementation
+        return a / b;
+    }
+
+    public int Maxfunc(int a, int b){
+        //To-Do implementation
+        return 0;
+    }
+    public int Meanfunc(int a, int b){
+        //To-Do implementation
+        return 0;
+    }
+    public int Minfunc(int a, int b){
+        //To-Do implementation
+        return 0;
+    }
+    public int Mulfunc(int a, int b){
+        //To-Do implementation
+        return 0;
+    }
+    public int Remainfunc(int a, int b){
+        //To-Do implementation
+        return 0;
+    }
+    public int Subfunc(int a, int b){
+        //To-Do implementation
+        return 0;
+    }
+
+}

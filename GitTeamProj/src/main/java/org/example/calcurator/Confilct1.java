46e03ef60104777dfe58278c20213bf7a5b4a243
윤동현 <eatpaper1429@gmail.com>
2023-03-11 15:45:11 +0900

    conflict1 수정

--- GitTeamProj/src/main/java/org/example/calcurator/Confilct1.java
+++ GitTeamProj/src/main/java/org/example/calcurator/Confilct1.java
@@ -5,12 +5,15 @@ public class Confilct1 {
   int left;
   int right;
 
-  public Confilct1(int left, int right) {
+  int center;
+
+  public Confilct1(int left, int right, int center) {
     this.left = left;
     this.right = right;
+    this.center = center;
   }
 
   public int result() {
-    return left + right;
+    return left + right + center;
   }
 }

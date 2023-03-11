a2fb3a87f72971a5c7ab9685d25e8b97036b1ba6
추호성 <cnghtjd3@naver.com>
2023-03-11 15:10:37 +0900

    #project-Test/8 Maxfunc 구현

--- GitTeamProj/src/main/java/org/example/calcurator/Maxfunc.java
+++ GitTeamProj/src/main/java/org/example/calcurator/Maxfunc.java
@@ -1,4 +1,21 @@
 package org.example.calcurator;
 
 public class Maxfunc {
+    int left;
+    int right;
+
+    public Maxfunc(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public int result() {
+        if (left >= right) {
+            System.out.println("result: " + left);
+            return left;
+        } else {
+            System.out.println("result: " + right);
+            return right;
+        }
+    }
 }

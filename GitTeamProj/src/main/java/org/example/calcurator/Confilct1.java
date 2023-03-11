bddcdeb14365d2bfa1a0f4df46face96008235b8
류승기 <ysgsl@naver.com>
2023-03-11 15:14:20 +0900

    conflict1 edit

--- GitTeamProj/src/main/java/org/example/calcurator/Confilct1.java
+++ GitTeamProj/src/main/java/org/example/calcurator/Confilct1.java
@@ -4,13 +4,15 @@ public class Confilct1 {
 
   int left;
   int right;
+  int mid;
 
-  public Confilct1(int left, int right) {
+  public Confilct1(int left, int right, int mid) {
     this.left = left;
     this.right = right;
+    this.mid = mid;
   }
 
   public int result() {
-    return left + right;
+    return left + right + mid;
   }
 }

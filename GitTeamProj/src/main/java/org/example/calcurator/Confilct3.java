ecd36687cf3c128c2cb492a656b5176d711f894d
추호성 <cnghtjd3@naver.com>
2023-03-11 16:00:14 +0900

    #conflict3 충돌 테스트1

--- GitTeamProj/src/main/java/org/example/calcurator/Confilct3.java
+++ GitTeamProj/src/main/java/org/example/calcurator/Confilct3.java
@@ -5,12 +5,16 @@ public class Confilct3 {
   int left;
   int right;
 
+  int middle;
+
   public Confilct3(int left, int right) {
     this.left = left;
     this.right = right;
+    this.middle = middel;
   }
 
   public int result() {
-    return left + right;
+    System.out.println("값: " + (left + right + middle));
+    return left + right + middle;
   }
 }

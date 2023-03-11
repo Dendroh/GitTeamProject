eb205b6d8eb3e6683b59e7d4753860bc64c0f42f
윤동현 <eatpaper1429@gmail.com>
2023-03-11 17:49:25 +0900

    new feature test

--- GitTeamProj/src/main/java/org/example/calcurator/Addfunc.java
+++ GitTeamProj/src/main/java/org/example/calcurator/Addfunc.java
@@ -12,6 +12,7 @@ public class Addfunc {
   public int result() {
     System.out.println("result");
     System.out.println("result2");
+    System.out.println("addtest");
     return left + right;
   }
 }
